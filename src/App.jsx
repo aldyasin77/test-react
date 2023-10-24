2e48b9c2091407a3a4f00a7309515e6dd6989ab6
aldyasin77 <aldyasin77@gmail.com>
2023-10-24 09:43:28 +0700

    Update Styling header landingpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="container">
+        <div>
+          <h1>
+            <a href="">Sins GROUP</a>
+          </h1>
+          <ul>
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Skill</a>
+            </li>
+            <li>
+              <a href="">Portofolio</a>
+            </li>
+            <li>
+              <a href="">Kontak</a>
+            </li>
+            <li>
+              <a href="">Kontak</a>
+            </li>
+          </ul>
+        </div>
+      </header>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
